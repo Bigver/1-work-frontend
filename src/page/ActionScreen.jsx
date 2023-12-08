9e57c3b135372f4d2e87cc63468fbf8c96596383
Bigver <91006041+Bigver@users.noreply.github.com>
2023-12-08 22:14:21 +0700

    Update ActionScreen.jsx

--- src/page/ActionScreen.jsx
+++ src/page/ActionScreen.jsx
@@ -6,6 +6,8 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Store } from '../Store.jsx';
 import { publicRequest } from "../publicRequest.js";
 import { toast } from 'react-toastify';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass , faUser ,  faHeart , faCheck  } from '@fortawesome/free-solid-svg-icons'
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -31,34 +33,100 @@ const reducer = (state, action) => {
 const ActionScreen = () => {
   const params = useParams();
   const { id } = params;
-  const [content, setContent] = useState([]);
-  useEffect(() => {
-    const getLocaion = async () => {
-      try  {
-        const res = await axios.get(`${publicRequest}/post/${id}`);
-        setContent(res.data);
-      } catch (err) {}
-    };
-    getLocaion();
-  }, []);
-    const { state, dispatch: ctxDispatch } = useContext(Store);
 
-  console.log(content)
+  const [{ loading, error, product, loadingCreateReview }, dispatch] =
+  useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`${publicRequest}/post/${id}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, [id]);
+
+
+  const navigate = useNavigate();
+  const [index, setIndex] = useState(0);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 0 : 1;
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity },
+    });
+    navigate('/cart');
+  };
+    const [learn, setLearn] = useState([]);
+    useEffect(() => {
+      const getLearn = async () => {
+        try  {
+          const res = await axios.get(`${publicRequest}/users/${userInfo._id}`);
+          setLearn(res.data);
+        } catch (err) {}
+      };
+      getLearn();
+    }, []);
+
+  const array1 = learn.learn || [];
+
+  const submitHandler = async (e) => {
+    array1.push(product._id)
+    try {
+      dispatch({ type: 'UPDATE_REQUEST' });
+      await axios.put(
+        `${publicRequest}/users/learn/${userInfo._id}`,
+        {
+          learn: array1
+         
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({
+        type: 'UPDATE_SUCCESS',
+      });
+      toast.success('เรียนแล้ว');
+      navigate('/');
+    } catch (err) {
+      toast.error(err);
+      dispatch({ type: 'UPDATE_FAIL' });
+    }
+  };
 
   return (
     <div className='action-ctn'>
       <div className="img">        
-        <img src={content.img} alt="" />
+        <img src={product.img} alt="" />
+        <div className="btn">
+          <button onClick={submitHandler}><FontAwesomeIcon icon={faCheck} style={{color: "#11ff00",}} />เรียนแล้ว</button>
+          <button onClick={addToCartHandler}><FontAwesomeIcon icon={faHeart} size="xxl" style={{color: "#ff0000",}} />favorite</button>
+        </div>
       </div>
         <div className="details">
             <div className="text">
-                <h1>{content.action}</h1>
-                <h2>{content.detail}</h2>
+                <h1>{product.action}</h1>
+                <h2>{product.detail}</h2>
                 <h1 className='d-1'>การฝึกปฎิบัติ</h1>
-                <h2>{content.practice}</h2>
+                <h2>{product.practice}</h2>
             </div>
             <iframe width="700" height="500"
-             src={content.vdo}>
+             src={product.vdo}>
             </iframe>
         </div>
     </div>
